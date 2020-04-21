afe0b2ff58ff432dcd232f5e1ed674d7956e4a83
endlessmeal <baloyan99@list.ru>
2020-04-21 17:37:41 +0300

    Just add Russia and World nav bar, works perfect with

--- app/src/main/java/com/perfectlogic/fragments/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/perfectlogic/fragments/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.perfectlogic.fragments.ui.dashboard;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +15,25 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.perfectlogic.fragments.R;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private TextView totalWorld;
+    private TextView totalText;
+
+    private TextView totalDead;
+    private TextView totalHealth;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,14 +41,79 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        totalWorld = root.findViewById(R.id.totalWorld);
+        totalText = root.findViewById(R.id.totalText);
 
-//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        totalHealth = root.findViewById(R.id.totalWorldHealth);
+        totalDead = root.findViewById(R.id.totalWorldDead);
+
+        GetWorldData gwd = new GetWorldData();
+        gwd.execute();
+
+        Date currentDay = Calendar.getInstance().getTime();
+        SimpleDateFormat dateFormat = null;
+        dateFormat = new SimpleDateFormat("dd MMMM", myDateFormatSymbols);
+        totalText.setText("Всего заболевших в Мире на " + dateFormat.format(currentDay));
+
         return root;
+    }
+
+    private static DateFormatSymbols myDateFormatSymbols = new DateFormatSymbols(){
+
+        @Override
+        public String[] getMonths() {
+            return new String[]{"января", "февраля", "марта", "апреля", "мая", "июня",
+                    "июля", "августа", "сентября", "октября", "ноября", "декабря"};
+        }
+
+    };
+
+    class GetWorldData extends AsyncTask<Void, Void, Void> {
+
+        String totalWorldDisease;
+        String totalWorldHealth;
+        String totalWorldDead;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Document doc = null;
+
+            try {
+                doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            Elements totalWorldByClass = doc.getElementsByClass("maincounter-number");
+            Elements totalWorldByTag = totalWorldByClass.tagName("span");
+            String total = totalWorldByTag.get(0).text();
+
+            Elements totalHealthByClass = doc.getElementsByClass("maincounter-number");
+            Elements totalHealthByTag = totalHealthByClass.tagName("span");
+            String total2 = totalHealthByTag.get(1).text();
+
+            Elements totalDeadByClass = doc.getElementsByClass("maincounter-number");
+            Elements totalDeadByTag = totalDeadByClass.tagName("span");
+            String total3 = totalDeadByTag.get(2).text();
+
+            if (total != null && total2 != null && total3 != null) {
+                totalWorldDisease = total;
+                totalWorldHealth = total2;
+                totalWorldDead = total3;
+            } else {
+                totalWorldDisease = "Error";
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            totalWorld.setText(totalWorldDisease);
+            totalHealth.setText(totalWorldHealth);
+            totalDead.setText(totalWorldDead);
+        }
     }
 }
--- app/src/main/java/com/perfectlogic/fragments/ui/home/HomeFragment.java
+++ app/src/main/java/com/perfectlogic/fragments/ui/home/HomeFragment.java
@@ -5,13 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.perfectlogic.fragments.R;
@@ -19,10 +16,8 @@ import com.perfectlogic.fragments.R;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.w3c.dom.Element;
 import org.w3c.dom.Text;
 
-import java.io.IOException;
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -33,30 +28,39 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private TextView totalRussia;
     private TextView totalText;
+
+    private TextView totalDead;
+    private TextView totalHealth;
+    private TextView totalTodayRussia;
+    private TextView totalTodayHealth;
+    private TextView totalTodayDead;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+
         totalRussia = root.findViewById(R.id.totalRussia);
         totalText = root.findViewById(R.id.totalText);
+
+        totalHealth = root.findViewById(R.id.totalHealth);
+        totalDead = root.findViewById(R.id.totalDead);
+
+        totalTodayRussia = root.findViewById(R.id.totalTodayRussia);
+        totalTodayHealth = root.findViewById(R.id.totalTodayHealth);
+        totalTodayDead = root.findViewById(R.id.totalTodayDead);
 
         GetData gd = new GetData();
         gd.execute();
 
+        //для установки русского отображения числа и месяца
         Date currentDay = Calendar.getInstance().getTime();
         SimpleDateFormat dateFormat = null;
         dateFormat = new SimpleDateFormat("dd MMMM", myDateFormatSymbols);
         totalText.setText("Всего заболевших в России на " + dateFormat.format(currentDay));
 
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
     }
 
@@ -72,7 +76,13 @@ public class HomeFragment extends Fragment {
 
     public class GetData extends AsyncTask<Void, Void, Void> {
 
-        String total;
+        String totalRussiaDisease;
+        String totalRussiaHealth;
+        String totalRussiaDead;
+
+        String dailyRussiaDisease;
+        String dailyRussiaHealth;
+        String dailyRussiaDead;
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -84,14 +94,44 @@ public class HomeFragment extends Fragment {
                 ex.printStackTrace();
             }
 
-            Elements element = doc.getElementsByClass("info-block disease");
-            Elements elem = element.select("i");
-            String elems = elem.text();
+            //получаем количество заболевших
+            Elements totalRussiaByClass = doc.getElementsByClass("info-block disease");
+            Elements totalRussiaByTag = totalRussiaByClass.select("i");
+            Elements dailyRussiaByTag = totalRussiaByClass.select("span");
+            String total = totalRussiaByTag.text();
+            String dailyTotal = dailyRussiaByTag.text();
 
-            if (elems != null) {
-                total = elems ;
+
+            //получаем количество выздоровевших
+            Elements totalHealthByClass = doc.getElementsByClass("info-block healed");
+            Elements totalHealthByTag = totalHealthByClass.select("i");
+            Elements dailyHealthByTag = totalHealthByClass.select("span");
+            String total2 = totalHealthByTag.text();
+            String dailyTotal2 = dailyHealthByTag.text();
+
+            //получаем количество погибших
+            Elements totalDeadByClass = doc.getElementsByClass("info-block deaths");
+            Elements dailyDeadByTag = totalDeadByClass.select("span");
+            Elements totalDeadByTag = totalDeadByClass.select("i");
+            String total3 = totalDeadByTag.text();
+            String dailyTotal3 = dailyDeadByTag.text();
+
+
+
+            if (total != null && total2 != null && total3 != null) {
+                totalRussiaDead = total3;
+                totalRussiaHealth = total2;
+                totalRussiaDisease = total;
             } else {
-                total = "Error";
+                totalRussiaDisease = "Error";
+            }
+
+            if (dailyTotal != null && dailyTotal2 != null && dailyTotal3 != null) {
+                dailyRussiaDisease = dailyTotal;
+                dailyRussiaHealth = dailyTotal2;
+                dailyRussiaDead = dailyTotal3;
+            } else {
+                dailyRussiaDisease = "Error";
             }
 
             return null;
@@ -101,7 +141,14 @@ public class HomeFragment extends Fragment {
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
 
-            totalRussia.setText(total);
+
+            totalDead.setText(totalRussiaDead);
+            totalHealth.setText(totalRussiaHealth);
+            totalRussia.setText(totalRussiaDisease);
+
+            totalTodayDead.setText(dailyRussiaDead);
+            totalTodayHealth.setText(dailyRussiaHealth);
+            totalTodayRussia.setText(dailyRussiaDisease);
         }
     }
 }
